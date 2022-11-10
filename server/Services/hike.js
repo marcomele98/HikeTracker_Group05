514a988a580226c215142ede741daf1507f53f74
Marco Mele s296538 <s296538@studenti.polito.it>
2022-11-10 10:51:50 +0100

    added controls

--- server/Services/hike.js
+++ server/Services/hike.js
@@ -10,10 +10,60 @@ class HikeDescription {
 
     constructor() { }
 
+
+    isNotValidBody = (data) => {
+        return data === undefined || data === null || data.length === 0;
+    }
+
+    isNotValidField = (field) => {
+        return field === undefined || field === '' || field === null;
+    }
+
+    isNotValidNumber = (number) => {
+        return number === undefined || number === '' || number === null || isNaN(number);
+    }
+
+    isNotValidPoint = (point) => {
+        let regexpLatitude = new RegExp('^-?([0-8]?[0-9]|90)(\.[0-9]{1,10})$');
+        let regexpLongitude = new RegExp('^-?([0-9]{1,2}|1[0-7][0-9]|180)(\.[0-9]{1,10})$');
+        return point.latitude === undefined || point.latitude === '' || 
+        point.latitude === null || !regexpLatitude.test(point.latitude) ||
+        point.longitude === undefined || point.longitude === '' || 
+        point.longitude === null || !regexpLongitude.test(point.longitude) ||
+        point.altitude === undefined || point.altitude === '' || 
+        point.altitude === null || isNaN(point.altitude);
+    }
+
     async newHikeDescription(req, res) {
         let hike = req.body;
         let lg_id = req.user.id;
-        // let end_point = hike.end_point
+        let role = req.user.role;
+
+        if(role !== "local guide"){
+            return res.status(401).end();
+        }
+
+
+        if (this.isNotValidBody(hike) || 
+            this.isNotValidField(hike.title) ||
+            this.isNotValidNumber(hike.length_kms)||
+            this.isNotValidNumber(hike.expected_mins)||
+            this.isNotValidNumber(hike.ascendent_meters)||
+            this.isNotValidField(hike.difficulty)||
+            this.isNotValidField(hike.region)||
+            this.isNotValidField(hike.city)||
+            this.isNotValidField(hike.gpx) ||
+            this.isNotValidPoint(hike.end_point) ||
+            this.isNotValidPoint(hike.start_point)) {
+            return res.status(422).end();
+        }
+
+        for (var i = 0; i < hike.reference_points.length; i++) {
+            if(this.isNotValidPoint(hike.reference_points[i])) {
+                return res.status(422).end();
+            }
+        }
+
         try {
             let hike_id = await db.newHike(hike, lg_id);
             let end_point_id = await pointDB.storePoint(hike.end_point, hike_id)
