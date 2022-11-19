62d7d1bb2b7a3e30fa9778374d50fc3f3878349e
Heba Hassan s296346 <s296346@studenti.polito.it>
2022-11-19 22:02:50 +0100

    implement backend_story5, insert hut

--- server/Services/huts.js
+++ server/Services/huts.js
@@ -2,27 +2,24 @@
 const hutDB = require('../Queries/hut');
 
 
-
-
-
-
-
 class HutDescription {
 
     constructor() { }
 
     isNotValidBody = (data) => {
-        return data == undefined;
+        return data === undefined || data === null || data.length === 0;
     }
 
     isNotValidField = (field) => {
-        return field == undefined;
+        return field === undefined || field === '' || field === null;
     }
-
-
 
     isNotValidProvince = (field) => {
         return field === undefined || field === '' || field === null || field.length !== 2;
+    }
+
+    isNotValidNumber = (number) => {
+        return number === undefined || number === '' || number === null || isNaN(number);
     }
 
     isNotValidPoint = (point) => {
@@ -37,6 +34,71 @@ class HutDescription {
             !regexpLongitude.test(point.longitude) ||
             point.altitude === undefined || point.altitude === '' ||
             point.altitude === null || isNaN(point.altitude);
+    }
+
+    async addHutDescription(req, res)
+    {
+        let hut = req.body;
+        let role = req.user.role;
+        let message = "";
+
+        if (role !== "local guide") {
+            return res.status(401).json("Not authenticated.");
+        }
+
+        if (this.isNotValidBody(hut)) {
+            message = "Invalid Body"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidField(hut.name)) {
+            message = "Invalid Name"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidField(hut.type)) {
+            message = "Invalid Type"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidPoint({...hut})) {
+            message = "Invalid point"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidField(hut.region)) {
+            message = "Invalid Region"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidProvince(hut.province)) {
+            message = "Invalid Province"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidField(hut.city)) {
+            message = "Invalid City"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidNumber(hut.number_of_beds)) {
+            message = "Invalid number_of_beds"
+            return res.status(422).json(message);
+        }
+
+        if (this.isNotValidField(hut.description)) {
+            message = "Invalid Description"
+            return res.status(422).json(message);
+        }
+
+        try {
+            await hutDB.addHut(hut);
+            return res.status(200).end();
+        }
+        catch (err) {
+            message = "Server Error"
+            return res.status(503).json(message);
+        }
     }
 
 
