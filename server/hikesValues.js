5f061dc1ebbbd5a5ed80706f91334a950671b375
Federico Rinaudi <federicorinaudi@gmail.com>
2022-11-19 14:05:21 +0100

    refactor

--- server/hikesValues.js
+++ server/hikesValues.js
@@ -6,7 +6,7 @@ const {li_file1, li_file2, li_file3, li_file4, li_file5, li_file6, li_file7, li_
 
 var hikevalues =
 [
-  [ 'Path to ROCCIAMELONE', 9, 420, 3538, 'Professional Hiker', 'Piemonte', 'TO', 'Mompantero', 1,
+  [ 'Path to Rocciamelone', 9, 420, 3538, 'Professional Hiker', 'Piemonte', 'TO', 'Mompantero', 1,
   pm_file1, 1, 'general point', 1, 'Hut point','A well-known route, much loved by Valsusini and beyond. It is long and challenging because of the difference in height, but very well signposted and above all with a support point about halfway'], //endpoint:1 pointtable,strtpoint:1 hut
   [ 'Ascesa al Rifugio Savona', 10.3, 244, 610, 'Hiker','Piemonte', 'CN', 'Garessio', 1,
   pm_file2, 1, 'Parking point', 1, 'Parking point','Intermediate Hiking Tour. Good fitness required. Mostly accessible paths. Sure-footedness required. The starting point of the Tour is right next to a parking lot'],//endpoint:1 parking,strtpoint:1 parking
@@ -24,7 +24,7 @@ var hikevalues =
   tn_file2,10,'general point',10,'general point','Loop hike between the spectacular Dolomite walls of the Latemar Massif. The tour takes its name from the leaning rock towers like the one in the Tuscan city.' ],
   ['The chromatic contrast of Corno Nero and Corno Bianco', 12.6,360,950,'Hiker','Trentino-Alto Adige','BZ','Aldino',1,
   tn_file3,11,'general point',11,'general point','Extremely panoramic hiking loop, which allows you to admire the Lagorai chain, the lower Val di Fiemme and to see the Val di Cembra in the distance. We are in Passo Oclini, where the road stops and you can only aim upwards towards the sky. The itinerary proposes the ascent of both peaks that frame the pass, but it is possible to go up just on one of them.'],
-  [ 'Two days in the mountains of Garda Trentino and 1 night in Rifugio Pernici',25.4,825,2086,'Professional Hiker','Trentino-Alto Adige','TN','Arco',1,
+  [ 'Two days in the mountains of Garda Trentino and one night in Rifugio Pernici',25.4,825,2086,'Professional Hiker','Trentino-Alto Adige','TN','Arco',1,
   tn_file4,15,'general point',15,'general point','Sleeping in a mountain hut is always a thrill. This excursion has an overnight stay in the mountains followed by a super panoramic hike back down to Riva del Garda'],
   [ 'Cima Cece',15.7,420,1100,'Professional Hiker','Trentino-Alto Adige','TN','Cavalese',1,
   tn_file5,19,'general point',19,'general point','The emotion of reaching the highest peak of the Lagorai.' ],
