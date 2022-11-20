0351f28b94b0921591cb8d9730f0870d192b2a12
giacomo13 <giacomomaggio13@gmail.com>
2022-11-20 11:27:12 +0100

    bug fixing

--- server/hutsValues.js
+++ server/hutsValues.js
@@ -16,11 +16,8 @@ var hutsvalues =
     ['Malga Valmaggiore','46.291392','11.646566','1610','Restaurant','Trentino-Alto Adige','TN','Cavalese'],
     ['Ponale Alto Belvedere','45.864402','10.833777','145','Restaurant','Trentino-Alto Adige','TN','Riva del Garda'],
     ['Agririfugio Molini', '44.31912', '9.178627', '232', 'Refuge', 'Liguria', 'GE', 'Fruttuoso', 11, 'Set on a hillside overlooking the Ligurian coast, this quiet, secluded farmhouse within Portofino Natural Park is only accessible via hiking trails.'],
-    //Valtournche23
     ['Rifúgio Barmasse', '45.87579','7.593994', '2174.2','Refuge','Valle d Aosta', 'AO', 'Fontanaz-Valmartin',25,''],
-    //Cervinia24
     ['Rifugio Duca degli Abruzzi all Oriondè', '45.958891','7.6441', '2798.2','Refuge','Valle d Aosta', 'AO', 'Breuil-Cervinia',22,''],
-    //Parco monte avic29
     ['Rifugio Barbustel', '45.649148', '7.588452', '2188', 'Refuge','Valle d Aosta', 'AO', 'Covarey', 40, '' ]
 ];
 module.exports.hutsvalues =   hutsvalues;
