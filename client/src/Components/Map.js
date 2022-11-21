bf3dddf2dddfe4b9e63770ae6a163de0560f7216
Marco Mele s296538 <s296538@studenti.polito.it>
2022-11-21 13:08:17 +0100

    fixed map when a gpx change

--- client/src/Components/Map.js
+++ client/src/Components/Map.js
@@ -1,11 +1,24 @@
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
-import { MapContainer, Polyline, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, Polyline, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import { ListGroupItem, ListGroup } from "react-bootstrap";
 import '../App.css';
 import markerIconPng from "leaflet/dist/images/marker-icon.png"
 import { Icon } from 'leaflet'
 let gpxParser = require('gpxparser');
+
+
+const MapMoover = ({position}) => {
+    const map = useMapEvents({})
+
+    React.useEffect(() => {
+        if ( position !== undefined){
+            console.log(position)
+            map.flyTo(position, map.getZoom())
+        }
+    }, [position])
+    return <></>
+}
 
 
 const Map = (props) => {
@@ -32,29 +45,30 @@ const Map = (props) => {
                             scrollWheelZoom={true}
                             style={{ height: "500px" }}
                         >
+                            <MapMoover position={{lat:positions[0][0], lng:positions[0][1]}}></MapMoover>
                             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
                             <Polyline
                                 pathOptions={{ fillColor: 'red', color: 'blue' }}
                                 positions={positions}
                             />
-                            {props.hike.points.map(p => <Marker key={[p.latitude, p.longitude, p.name]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
+                            {props.hike.points.map(p => <Marker key={[p.latitude, p.longitude, p.name, p.altitude]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
                                 console.log(marker)
                                 <Popup>
-                                    {p.name ? ("Name: " + p.name) : ""} {p.name ? <br /> : false} lat: {p.latitude} <br /> lon: {p.longitude}
+                                    {p.name ? ("Name: " + p.name) : ""} {p.name ? <br /> : false} Latitude: {p.latitude} <br /> Longitude: {p.longitude} <br /> Altitude: {p.altitude + " m"}
                                 </Popup>
                             </Marker>)
                             }
-                            {props.hike.huts.map(p => <Marker key={[p.latitude, p.longitude, p.name]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
+                            {props.hike.huts.map(p => <Marker key={[p.latitude, p.longitude, p.name, p.altitude]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
                                 console.log(marker)
                                 <Popup>
-                                    Name: {p.name} <br /> lat: {p.latitude} <br /> lon: {p.longitude}
+                                    Name: {p.name} <br /> Latitude: {p.latitude} <br /> Longitude: {p.longitude} <br /> Altitude: {p.altitude + " m"}
                                 </Popup>
                             </Marker>)
                             }
-                            {props.hike.parking_lots.map(p => <Marker key={[p.latitude, p.longitude, p.name]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
+                            {props.hike.parking_lots.map(p => <Marker key={[p.latitude, p.longitude, p.name, p.altitude]} position={[p.latitude, p.longitude, p.name]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
                                 console.log(marker)
                                 <Popup>
-                                    Name: {p.name} <br /> lat: {p.latitude} <br /> lon: {p.longitude}
+                                    Name: {p.name} <br /> Latitude: {p.latitude} <br /> Longitude: {p.longitude} <br /> Altitude: {p.altitude + " m"}
                                 </Popup>
                             </Marker>)
                             }
