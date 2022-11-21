a11ff8fbe9899952438738d8730c6b99c0472ccd
Marco Mele s296538 <s296538@studenti.polito.it>
2022-11-21 18:31:22 +0100

    fixing

--- server/Queries/point.js
+++ server/Queries/point.js
@@ -3,7 +3,7 @@
 const db = require('./DAO');
 const sqlite = require('sqlite3');
 const db1 = new sqlite.Database('HT.sqlite', err => { if (err) throw err;});
-const {PointStruct} = require("../Models/point_model");
+// const {PointStruct} = require("../Models/point_model");
 
 exports.storePoint = async (point, hike_id) => {
     const sql = 'INSERT INTO POINT(latitude, longitude, altitude, name, address, hike_id) VALUES(?, ?, ?, ?, ?, ?)'
@@ -27,9 +27,9 @@ exports.getPointById = (id) => {
         } else if (row === undefined) {
           resolve(-1);
         } else {
-          const Point = new PointStruct(row.id,row.latitude,row.longitude,row.altitude, row.name, 
-                                 row.address, row.hike_id);  
-          resolve(Point);
+          // const Point = new PointStruct(row.id,row.latitude,row.longitude,row.altitude, row.name, 
+          //                        row.address, row.hike_id);  
+          resolve(row);
         }
       });
     });
