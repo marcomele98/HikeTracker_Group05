7343db40fd62a893b7bf3bed6769157874fd6a01
giacomo13 <giacomomaggio13@gmail.com>
2022-11-22 12:32:33 +0100

    test finished

--- server/unit_test/huts_lits.test.js
+++ server/unit_test/huts_lits.test.js
@@ -1,12 +1,19 @@
-const hike = require('../Queries/hike');
+const hut = require('../Queries/hut');
 const db = require('../Queries/DAO');
 
 
-
-
-function HikesHuts(hike_id, hut_id) {
-    this.hike_id = hike_id;
-    this.hut_id = hut_id;
+function Hut(id, name, latitude, longitude, altitude, type, region, province, city, number_of_beds, description){
+    this.id = id;
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    this.altitude = altitude;
+    this.type = type;
+    this.region = region;
+    this.province = province;
+    this.city = city;
+    this.number_of_beds = number_of_beds;
+    this.description = description;
 }
 
 
@@ -20,25 +27,10 @@ describe("hutsDao", () => {
         await db.run('DELETE FROM USER');
         await db.run('DELETE FROM sqlite_sequence');
         await db.run(
-            "INSERT OR IGNORE INTO USER(name, surname, role, password, email, salt, phone_number)\
-               VALUES ('Mario', 'Rossi', 'local guide', \
-                      'df34c7212613dcb7c25593f91fbb74fb99793a440a2b9fe8972cbadb0436a333', \
-                      'lg1@p.it', '4783473632662333', '3334567980')"
-        );
-
-        await db.run("INSERT INTO HIKE(id, title, length_kms, expected_mins, ascendent_meters, difficulty, description, region, province, city, lg_id, gpx, end_point, end_point_type, start_point, start_point_type)\
-            VALUES (1, 'ROCCIAMELONE', 9, 420, 3538, 'Professional Hiker', '', 'Piemonte', 'TO', 'Montepantero', 1, 'gpx_content', 1, 'point', 2, 'parking_lot'),\
-            (2, 'Salita al Monte Antoroto', 17, 444, 400, 'Professional Hiker', '', 'Piemonte', 'CN', 'Garessio', 1, 'gpx_content', 1, 'parking_lot', 3, 'parking_lot')"
-        );
-        await db.run(
             "INSERT INTO HUT(id,name,latitude, longitude, altitude, type, region, province, city, number_of_beds, description)\
-                 VALUES (1, 'Refuge La Riposa','45.17778', '7.08337', '2185','Refuge', 'Piemonte', 'TO','Mompantero', 20, ''),\
-                (2, 'Refugio Asti'   ,'45.19177', '7.07427','2854','Refuge', 'Piemonte', 'TO','Mompantero', 15, '')"
-        );
-
-        await db.run("INSERT INTO HIKE_HUT (hike_id, hut_id)\
-        VALUES(1, 1),\
-        (2, 2)"
+                 VALUES (1, 'Refuge La Riposa','45.17778', '7.08337', '2185','Refuge', 'Piemonte', 'TO','Mompantero', 20, 'prova1'),\
+                        (2, 'Refugio Asti'   ,'45.19177', '7.07427','2854','Refuge', 'Piemonte', 'TO','Mompantero', 15, 'prova2'),\
+                        (3, 'Rifugio Duca degli Abruzzi','45.958891','7.6441','2798.2','Refuge','Valle d Aosta','AO','Breuil-Cervinia',22,'prova3')"
         );
     });
 
@@ -49,5 +41,44 @@ describe("hutsDao", () => {
         await db.run('DELETE FROM USER');
         await db.run('DELETE FROM sqlite_sequence');
 
+    });
+
+    test('test getAllHuts', async () => {
+        let data = await hut.getHuts();
+        let hut1 = new Hut(1, 'Refuge La Riposa','45.17778', '7.08337', '2185','Refuge', 'Piemonte', 'TO','Mompantero', 20, 'prova1');
+        let hut2 = new Hut(2, 'Refugio Asti'   ,'45.19177', '7.07427','2854','Refuge', 'Piemonte', 'TO','Mompantero', 15, 'prova2');
+        let hut3 = new Hut(3, 'Rifugio Duca degli Abruzzi','45.958891','7.6441','2798.2','Refuge','Valle d Aosta','AO','Breuil-Cervinia',22,'prova3');
+        expect(data.length).toStrictEqual(3);
+        let hut_check1 = new Hut(data[0].id, data[0].name, data[0].latitude, data[0].longitude, data[0].altitude, data[0].type, data[0].region, data[0].province, data[0].city, data[0].number_of_beds, data[0].description);
+        let hut_check2 = new Hut(data[1].id, data[1].name, data[1].latitude, data[1].longitude, data[1].altitude, data[1].type, data[1].region, data[1].province, data[1].city, data[1].number_of_beds, data[1].description);
+        let hut_check3 = new Hut(data[2].id, data[2].name, data[2].latitude, data[2].longitude, data[2].altitude, data[2].type, data[2].region, data[2].province, data[2].city, data[2].number_of_beds, data[2].description);
+        expect(hut1).toEqual(hut_check1);
+        expect(hut2).toEqual(hut_check2);
+        expect(hut3).toEqual(hut_check3);
+    });
+
+    test('test getHutById', async () => {
+        let data;
+        let hut1 = new Hut(1, 'Refuge La Riposa','45.17778', '7.08337', '2185','Refuge', 'Piemonte', 'TO','Mompantero', 20, 'prova1');
+        let hut2 = new Hut(2, 'Refugio Asti'   ,'45.19177', '7.07427','2854','Refuge', 'Piemonte', 'TO','Mompantero', 15, 'prova2');
+        let hut3 = new Hut(3, 'Rifugio Duca degli Abruzzi','45.958891','7.6441','2798.2','Refuge','Valle d Aosta','AO','Breuil-Cervinia',22,'prova3');
+
+        data = await hut.getHutById(1);
+        let hut_check1 = new Hut(data.id, data.name, data.latitude, data.longitude, data.altitude, data.type, data.region, data.province, data.city, data.number_of_beds, data.description);
+
+        data = await hut.getHutById(2);
+        let hut_check2 = new Hut(data.id, data.name, data.latitude, data.longitude, data.altitude, data.type, data.region, data.province, data.city, data.number_of_beds, data.description);
+
+        data = await hut.getHutById(3);
+        let hut_check3 = new Hut(data.id, data.name, data.latitude, data.longitude, data.altitude, data.type, data.region, data.province, data.city, data.number_of_beds, data.description);
+        expect(hut1).toEqual(hut_check1);
+        expect(hut2).toEqual(hut_check2);
+        expect(hut3).toEqual(hut_check3);
+
+        data = await hut.getHutById(-1);
+        expect(data).toBe(undefined);
+
+        data = await hut.getHutById(4);
+        expect(data).toBe(undefined);
     });
 })
