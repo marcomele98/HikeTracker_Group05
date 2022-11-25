b83ad57406d3ca7d8e47697e59880f784ac91d6b
mattiascamuzzi <s303388@studenti.polito.it>
2022-11-25 11:13:28 +0100

    delete all users on deleteDB

--- server/deleteDB.js
+++ server/deleteDB.js
@@ -5,30 +5,30 @@ const fs = require('fs');
 const DBPath = "./HT.sqlite";
 
 var admin = require("firebase-admin");
-
 var serviceAccount = require("./admin.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-try {
-  admin.auth().getUserByEmail("lg1@p.it")
-  .then(function (user) {
-    if(user != undefined) {
-      admin.auth().deleteUser(user.uid);
-    }
-  });
-} catch {}
+var getAuth = admin.auth();
 
-try {
-  admin.auth().getUserByEmail("h1@p.it")
-  .then(function (user) {
-    if(user != undefined) {
-      admin.auth().deleteUser(user.uid);
+const deleteAllUsers = (nextPageToken) => {
+  getAuth.listUsers(1000, nextPageToken)
+  .then((listUsersRes) => {
+    listUsersRes.users.forEach((user) => {
+      getAuth.deleteUser(user.toJSON().uid);
+    });
+    if (listUsersRes.pageToken) {
+      deleteAllUsers(listUsersRes.pageToken);
     }
+  })
+  .catch((error) => {
+    console.log(error);
   });
-} catch {}
+};
+
+deleteAllUsers();
 
 if(fs.existsSync(DBPath))
   fs.unlinkSync(DBPath)
