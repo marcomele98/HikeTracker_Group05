5f05ede5851d8e81acf0f35d91624c9f84c11e96
giacomo13 <giacomomaggio13@gmail.com>
2022-11-25 17:27:33 +0100

    added test for updateHike function

--- server/unit_test/link_new_start_arrival.test.js
+++ server/unit_test/link_new_start_arrival.test.js
@@ -205,6 +205,7 @@ describe("Update start/arrival for an hike", () => {
         
         let hike_data;
         let data_check;
+        let log;
 
         const park_data = await parking.getParkingById(1); //it will be the start point
         const hut_data = await hut.getHutById(1); //it will be the end point
@@ -216,8 +217,9 @@ describe("Update start/arrival for an hike", () => {
         data_check = await point.getPointById(hike_data.end_point);
         expect(data_check).not.toEqual(hut_data);
 
+
         //after changes
-        const log = hike.updateHike(hut_data.id,'Hut point',park_data.id,'Parking point',2);
+        log = await hike.updateHike(hut_data.id,'Hut point',park_data.id,'Parking point',2);
         hike_data = await hike.getHikeById(2);
         
         //checking types
@@ -225,12 +227,29 @@ describe("Update start/arrival for an hike", () => {
         expect(hike_data.end_point_type).toEqual('Hut point');
         
         //checking point objects
-        const new_start_point = await parking.getParkingById(hike_data.start_point);
-        const new_end_point = await hut.getHutById(hike_data.end_point);
+        let new_start_point = await parking.getParkingById(hike_data.start_point);
+        let new_end_point = await hut.getHutById(hike_data.end_point);
         expect(park_data).toEqual(new_start_point);
         expect(hut_data).toEqual(new_end_point);
 
 
+        //try to change just the end point to make a ring
+        log = await hike.updateHike(park_data.id,'Parking point',park_data.id,'Parking point',2);
+        hike_data = await hike.getHikeById(2);
+
+        //checking types
+        expect(hike_data.start_point_type).toEqual('Parking point');
+        expect(hike_data.end_point_type).toEqual('Parking point');
+        
+        //checking point objects
+        new_start_point = await parking.getParkingById(hike_data.start_point);
+        new_end_point = await parking.getParkingById(hike_data.start_point);
+        expect(park_data).toEqual(new_start_point);
+        expect(park_data).toEqual(new_end_point);
+        expect(hut_data).not.toEqual(new_end_point);
+        expect(hut_data).not.toEqual(new_start_point);
+
+        
     });
 
 });
