153f6efb1934196733fa0f6e5b575d10178b3801
Felicia799 <s297980@studenti.polito.it>
2022-11-26 18:02:47 +0100

    fix link_start_arrival

--- client/cypress/e2e/link_start_arrival.cy.js
+++ client/cypress/e2e/link_start_arrival.cy.js
@@ -8,19 +8,17 @@ describe('loginform e2e tset', () => {
         cy.contains('Login').click()
     })
 
-
     it('login form test',()=>{
         cy.get('input[id=username]').clear()
         cy.get('input[id=password]').clear()
         cy.get('input[id=username]').type('lg1@p.it').should('have.value', 'lg1@p.it')
         cy.get('input[id=password]').type('password').should('have.value', 'password')
         cy.contains('Login').click()
-        cy.url().should('include', '/home')
-        
+        cy.url().should('include', '/home')    
     })
 })
-describe('get the hikes list', () => { 
 
+describe('get the hikes list', () => { 
     it('jump into certain hike page test',()=>{
         cy.contains('Great War: from Malga Grassi to Rifugio Pernici').parent().parent()
         .within(() => {     
@@ -36,16 +34,14 @@ describe('get the hikes list', () => {
     })
 
     it('adding hut point as start point test',() =>{
-        cy.get('select').select('hut').should('have.value', 'hut')
-       // cy.contains('Rifugio Capanna')
-        //cy.get('select').select('Rifugio Capanna').should('have.value', 'Rifugio Capanna')
+        cy.contains('hut')
         cy.contains('Confirm').click()
-        //cy.get('.pointTitle').contains('Rifugio Capanna')
+        
     })
     
     it('start point change successfully test',()=>{
         cy.visit('http://localhost:3000/hike/7')
-       cy.get('.pointTitle').contains('Rifugio Capanna (Refuge)')
+       cy.get('.pointTitle').contains('Rifugio Nino Pernici (Refuge)')
     })
 })
 
@@ -80,21 +76,15 @@ describe('cancel button test', ()=>{
   
     it('Cancel button test',()=>{
         cy.contains('Start Point').parent()
-        // .within(() => {     
-        //     cy.get('button').click()
-        //     })
-           cy.contains('Cancel').click()
-      
+        cy.contains('Cancel').click() 
     })
 })
-//test confirm button when adding  a parking lot as start point of the hike
-describe('adding a parking lot as start point test', ()=>{
+// //test confirm button when adding  a parking lot as start point of the hike
+describe('adding a hut as end point test', ()=>{
     it('start point change successfully test',()=>{
         cy.visit('http://localhost:3000/hike/7')
       
     })
-
-
 
     it('Login button exists',()=>{
         cy.contains('Login').click()
@@ -107,8 +97,8 @@ describe('adding a parking lot as start point test', ()=>{
         cy.get('input[id=password]').type('password').should('have.value', 'password')
         cy.contains('Login').click()
         cy.url().should('include', '/home')
-        
     })
+
     it('jump into certain hike page test',()=>{
         cy.contains('Great War: from Malga Grassi to Rifugio Pernici').parent().parent()
         .within(() => {     
@@ -117,18 +107,17 @@ describe('adding a parking lot as start point test', ()=>{
     })  
     
     it('edit button test',() => {
-        cy.contains('Start Point').parent()
+        cy.contains('End Point').parent()
         .within(() => {     
             cy.get('button').click()
             })
     })
   
-    it('adding hut point as start point test',() =>{
-        cy.get('select').select('parking lot').should('have.value', 'parking lot')
-       // cy.contains('Rifugio Capanna')
-        //cy.get('select').select('Rifugio Capanna').should('have.value', 'Rifugio Capanna')
+    it('adding hut as end point test',() =>{
+        cy.contains('hut')
         cy.contains('Confirm').click()
-        //cy.get('.pointTitle').contains('Rifugio Capanna')
+      
+       
     })
  
 })
