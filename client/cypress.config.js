a835e15bee96e43a5dd74a1c4544b007200ef6cb
Federico Rinaudi <federicorinaudi@gmail.com>
2022-12-05 10:25:51 +0100

    try resolving sonar cloud bug

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -3,18 +3,15 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 
   "reporter": "mochawesome",
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  }
-}),
-{
- // "reporter": "mochawesome",
   "reporterOptions": {
     "reportDir": "cypress/results",
     "overwrite": false,
     "html": false,
     "json": true
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   }
-}
+})
