6a358401f71b28b19e5a82875771a506cbd0b226
Felicia799 <s297980@studenti.polito.it>
2022-12-06 16:07:16 +0100

    Encapsulating the login function and combining all the tests on hut in one file

---
+++ client/cypress/e2e/testHut.cy.js
@@ -0,0 +1,77 @@
+/*this file include e2eTest of inserting hut(story5) and searching hut(story7),
+in this way can reduce some repeated steps*/
+//test new hut(story5)
+describe('hutform e2e tset', () => {
+    it('login ', () => {
+         cy.login("lg1@p.it","password")
+    })
+
+    it('jump into Huts page test',()=>{
+        cy.contains('Huts').click({force: true})
+        cy.url().should('include','/huts')
+      
+    })    
+    
+    it('create new huts button test',()=>{
+        cy.contains("New Hut").click({force: true})
+        cy.url().should('include', '/new-hut')
+   })
+
+    it('submit hut form',()=>{
+        cy.get('.list-group').trigger('click',367,90)
+        cy.get('input[id=validationCustom01]').clear({force: true}).type('Torino Lingotto').should('have.value', 'Torino Lingotto')
+        cy.get('input[id=validationCustom08]').clear({force: true}).type('Refuge').should('have.value', 'Refuge')
+        cy.get('input[id=validationCustom09]').clear({force: true}).type('40').should('have.value', '40')
+        cy.get('input[id=validationCustom03]').clear({force: true}).type('200').should('have.value', '200')
+        cy.get('textarea').clear({force: true}).type('good').should('have.value', 'good')
+        cy.get("[type='submit']").click({ force: true })
+    })
+     
+})
+
+//testwrong insertion with empty info and cancel button
+describe('cancel button test of hutform e2e tset', () => {
+    it('jump into Huts page test',()=>{
+        cy.contains("New Hut").click({force: true})     
+    })    
+
+    it('wrong insertion with empty infomation',()=>{
+        cy.contains('Create new hut').click()
+        cy.contains('Please insert name')
+        cy.contains('Please insert correct type')
+        cy.contains('Please insert correct number of beds')
+        cy.contains('Please insert correct description')
+        cy.contains('Please insert correct altitude')
+    })
+    
+        it('Cancel button test',()=>{
+        cy.contains('Cancel').click()
+        cy.url().should('include', '/huts')
+    })
+})
+
+//test searchHut function(story7)
+describe('search hut e2e tset', () => { 
+    it('search input form test',()=>{
+       cy.get(".md[type='text']").type('Vil',{force: true}).should('have.value','Vil')
+       cy.get('.list-group').should('contain','Villaggio Baffa (Hotel)','Calabria','Cotronei (KR)')
+    })
+
+    it('see more button exist',()=>{
+       cy.contains('see more').click()
+       cy.url().should('include','hut/18')
+    })
+
+   it('show all info of the hut which is searched',()=>{
+       cy.get('.titleBig').should('contain','Villaggio Baffa (Hotel)')
+       cy.get('.textGrayPrimaryItalic').should('contain','Villaggio Baffa is an excellent combination for short or long stays and at any time of the year.')
+       cy.get('.textGrayPrimaryBig').should('contain','Calabria','Cotronei (KR)','Beds: 28')
+    })
+
+   it('the input does not match any hut test',()=>{
+       cy.contains('Huts').click({force: true})
+       cy.get(".md[type='text']").type('Ro',{force: true}).should('have.value','Ro')
+       cy.get('.container').should("not.have.class","list-group")
+   })
+
+})
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -24,6 +24,18 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("getByTestId", (testId) =>{
-    cy.get(`[data-cy='${testId}']`)
-})
+//将用户名和密码进行登录参数化
+Cypress.Commands.add("login",(username,password) => {
+    cy.clearCookies() //清除cookies,保证页面为未登录状态
+    cy.visit('http://localhost:3000/') //访问url
+    cy.url().should('include', 'http://localhost:3000/') //验证目标url 是否正确包含光谷社区正确域名 验证是否正确跳转到光谷社区页面
+    
+    cy.contains('Login').click() //点击登录按钮
+    cy.url().should('include', 'login') //验证正确跳转到登录页面
+    cy.get('input[id=username]') //根据 css 定位用户名输入框
+    .type(username) //输入参数化的用户名
+    cy.get('input[id=password]') //根据 css 定位密码输入框
+    .type(password) //输入参数化的密码
+    cy.get('#submitLogin').click() //点击登录按钮
+    cy.contains('Logout').should('be.visible') 
+    })
