2dea8b3ac2c80c1f783e01b7e3e13234d48de89c
Heba Hassan s296346 <s296346@studenti.polito.it>
2023-01-08 15:37:07 +0100

    reduce codesmells

--- server/Services/preferences.js
+++ server/Services/preferences.js
@@ -28,61 +28,7 @@ class Preferences{
             return res.status(401).json("Not authenticated.");
         }
 
-        if (servicesUtility.isNotValidBody(preference)) {
-            message = "Invalid Body"
-        }
-
-        if (preference.max_length_kms && servicesUtility.isNotValidNumber(preference.max_length_kms)) {
-            message = "Invalid Maximum Length"
-        }
-
-        if (preference.min_length_kms && servicesUtility.isNotValidNumber(preference.min_length_kms)) {
-            message = "Invalid Minimum Length"
-        }
-
-        if (preference.max_expected_mins && servicesUtility.isNotValidNumber(preference.max_expected_mins)) {
-            message = "Invalid Maximum Expected Time"
-        }
-
-        if (preference.min_expected_mins && servicesUtility.isNotValidNumber(preference.min_expected_mins)) {
-            message = "Invalid Minimum Expected Time"
-        }
-
-        if (preference.max_ascendent_meters && servicesUtility.isNotValidNumber(preference.max_ascendent_meters)) {
-            message = "Invalid Maximum Ascent"
-        }
-
-        if (preference.min_ascendent_meters && servicesUtility.isNotValidNumber(preference.min_ascendent_meters)) {
-            message = "Invalid Minimum Ascent"
-        }
-
-        if (preference.max_difficulty && servicesUtility.isNotValidDiff(preference.max_difficulty)) {
-            message = "Invalid Maximum Difficulty"
-        }
-
-        if (preference.min_difficulty && servicesUtility.isNotValidDiff(preference.min_difficulty)) {
-            message = "Invalid Minimum Difficulty"
-        }
-
-        if (preference.radius && servicesUtility.isNotValidNumber(preference.radius)) {
-            message = "Invalid Radius"
-        }
-
-        if (preference.region && servicesUtility.isNotValidField(preference.region)) {
-            message = "Invalid Region"
-        }
-
-        if (preference.province && servicesUtility.isNotValidProvince(preference.province)) {
-            message = "Invalid Province"
-        }
-
-        if (preference.city && servicesUtility.isNotValidField(preference.city)) {
-            message = "Invalid City"
-        }
-
-        if (preference.point_latitude && preference.point_longitude && servicesUtility.isNotValidPointUser({...preference})) {
-            message = "Invalid point"
-        }
+        message = prefrencesStructCheck(preference);
 
         if(message){
             return res.status(422).json(message);
@@ -110,6 +56,81 @@ class Preferences{
     }
 
 
+}
+
+function prefrencesStructCheck (preference)
+{
+    let message = ""
+    let message1 = ""
+
+    if (servicesUtility.isNotValidBody(preference)) {
+        message = "Invalid Body"
+    }
+
+    if (preference.max_length_kms && servicesUtility.isNotValidNumber(preference.max_length_kms)) {
+        message = "Invalid Maximum Length"
+    }
+
+    if (preference.min_length_kms && servicesUtility.isNotValidNumber(preference.min_length_kms)) {
+        message = "Invalid Minimum Length"
+    }
+
+    if (preference.max_expected_mins && servicesUtility.isNotValidNumber(preference.max_expected_mins)) {
+        message = "Invalid Maximum Expected Time"
+    }
+
+    if (preference.min_expected_mins && servicesUtility.isNotValidNumber(preference.min_expected_mins)) {
+        message = "Invalid Minimum Expected Time"
+    }
+
+    if (preference.max_ascendent_meters && servicesUtility.isNotValidNumber(preference.max_ascendent_meters)) {
+        message = "Invalid Maximum Ascent"
+    }
+
+    if (preference.min_ascendent_meters && servicesUtility.isNotValidNumber(preference.min_ascendent_meters)) {
+        message = "Invalid Minimum Ascent"
+    }
+
+    message1 = prefrencesLocationCheck (preference);
+
+    (message1) ? message =  message1 : false;
+
+    return message;
+}
+
+function prefrencesLocationCheck (preference)
+{
+    let message = ""
+
+    if (preference.max_difficulty && servicesUtility.isNotValidDiff(preference.max_difficulty)) {
+        message = "Invalid Maximum Difficulty"
+    }
+
+    if (preference.min_difficulty && servicesUtility.isNotValidDiff(preference.min_difficulty)) {
+        message = "Invalid Minimum Difficulty"
+    }
+
+    if (preference.radius && servicesUtility.isNotValidNumber(preference.radius)) {
+        message = "Invalid Radius"
+    }
+
+    if (preference.region && servicesUtility.isNotValidField(preference.region)) {
+        message = "Invalid Region"
+    }
+
+    if (preference.province && servicesUtility.isNotValidProvince(preference.province)) {
+        message = "Invalid Province"
+    }
+
+    if (preference.city && servicesUtility.isNotValidField(preference.city)) {
+        message = "Invalid City"
+    }
+
+    if (preference.point_latitude && preference.point_longitude && servicesUtility.isNotValidPointUser({...preference})) {
+        message = "Invalid point"
+    }
+
+    return message;
 }
 
 module.exports.Preferences = Preferences;
